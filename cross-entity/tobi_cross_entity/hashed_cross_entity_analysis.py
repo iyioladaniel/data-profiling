1a3d47ef7078471b25085867fef80b3333c4812f
Tobaaaaay <45228402+Tobaaaaay@users.noreply.github.com>
2025-03-19 21:39:00 +0000

    minor fixes - function names, array declaration

--- cross-entity/tobi_cross_entity/hashed_cross_entity_analysis.py
+++ cross-entity/tobi_cross_entity/hashed_cross_entity_analysis.py
@@ -1,16 +1,15 @@
 # This script calculates the count of common elements across a specified number of files (N),
 # considering all possible combinations of files ranging from 1 to N.
-
 # Usage: python3 ./overlapping_customer_analysis_name.py
-# PS: the file names are configured manually in the function possible_combinations()
+# PS: the file names are configured manually in the function generate_possible_combinations()
 
 from itertools import combinations
 import pandas as pd
 
 # Function to create the all possible combinations of the files
-def possible_combinations():
+def generate_possible_combinations():
+    combinations_list = []
     for i in range(1,5):
-        
         # input files names into the items[] array
         # items = ["entity1".txt", "entity2.txt","entity3.txt", "entity4.txt","entity5.txt"]
         items = [
@@ -19,11 +18,12 @@ def possible_combinations():
             "securities.bvn.sorted.unique.cleaned.hashed",
             "trustees.bvn.sorted.unique.cleaned.hashed"
             ]
-        combs = list(combinations(items,i))  
-        print(combs)  # [('A', 'B', 'C'), ('A','D'), ('A', 'C', 'D', B')]
-        print(str(len(combs))+ " combibations created") 
+        computed_combinations = list(combinations(items,i))  
+        combinations_list = combinations_list + computed_combinations
+    print(combinations_list)  # [('A', 'B', 'C'), ('A','D'), ('A', 'C', 'D', B')]
+    print(str(len(combinations_list))+ " combibations created") 
 
-    return handle_nested(combs)
+    return review_file_groups(combinations_list)
     
 # Function to read files  
 def read_file(file_path):
@@ -31,9 +31,9 @@ def read_file(file_path):
         # return set(file.read().split(','))
         return set(file.read().splitlines())
 
-# Function to manage the analysis of file combinations grouped in the nested data structure from the possible_combinations() method
-def handle_nested(combs):
-    for i in combs:
+# Function to manage the analysis of file combinations grouped in the nested data structure from the generate_possible_combinations() method
+def review_file_groups(combinations_list):
+    for i in combinations_list:
         # To test output
         # print(str(i))
         for x,file in enumerate(i):
@@ -65,8 +65,7 @@ def update_data_structure(file_names,quantity,intersections):
 
 # main function
 def main():
-
-    possible_combinations()     
+    generate_possible_combinations()     
     print(df) 
     df.to_csv('Overlapping_Analysis.csv', index = True) 
 
